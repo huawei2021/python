3f426d8072fba75253cde14f351aa21571810924
huawei2021 <79637825+huawei2021@users.noreply.github.com>
2021-03-06 18:13:02 +0800

    Add files via upload

---
+++ 0305-1,2.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  5 20:15:32 2021
+
+@author: USER
+"""
+
+'''
+1.六個數以氣泡排序法排列
+2.1~49取6個數不重複
+'''
+
+      
+
+
+import random
+a=list()
+for i in range(1,50):
+    a.append(i)
+n=random.sample(a,6)
+
+# while len(n)<4:
+#     num=int(input('請輸入數字:'))
+#     n.append(num)
+print('串列內容:',n)
+print('氣泡排序法:')
+l=len(n)
+for i in range(l)[::-1]:
+    for j in range(i):
+        if n[j]>n[j+1]:
+            k=n[j]
+            n[j]=n[j+1]
+            n[j+1]=k
+print(n)
+
+
+
+
+
+
+
